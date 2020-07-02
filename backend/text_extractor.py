63ce01329a5d0f55f1d642779409bbc0ec78cbb8
Jada <jadal115@gmail.com>
2020-07-01 20:57:49 -0500

    Print message if text extractor finds no files

--- backend/text_extractor.py
+++ backend/text_extractor.py
@@ -6,12 +6,16 @@ from PIL import Image
 #     def extract_text(img):
 pytesseract.pytesseract.tesseract_cmd = r"C:\Users\Jeremy\AppData\Local\Tesseract-OCR\tesseract.exe"
 
+files = glob.glob("dsfasdf/test_photos/*")
 
-for filename in glob.glob("backend/test_photos/*"):
-    print(filename)
-    image = Image.open(filename)
-    text = pytesseract.image_to_string(image)
-    print(text)
+if len(files) == 0:
+    print(f"No files found")
+else:
+    for filename in files:
+        print(filename)
+        image = Image.open(filename)
+        text = pytesseract.image_to_string(image)
+        print(text)
 # text = pytesseract.image_to_string(Image.open(r'C:\Users\Jeremy\Documents\Code\acnh-item-ocr\backend\test_photos\20200625_175157.jpg'))
 # print(text)
 
